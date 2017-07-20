1ee7ac6808dc8fa094cf8d9c16137d1eb7ff1532
WILLHELM LEHMAN <wwlehman@gmail.com>
2017-07-19 17:32:01 -0700

    — Updated test code: though it does not do anything just yet — added logback

--- src/test/java/hello/SampleAsyncProjectApplicationTests.java
+++ src/test/java/hello/SampleAsyncProjectApplicationTests.java
@@ -2,12 +2,12 @@ package hello;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SampleAsyncProjectApplication.class)
+@ContextConfiguration(classes = SampleAsyncProjectApplication.class)
 @WebAppConfiguration
 public class SampleAsyncProjectApplicationTests {
 
